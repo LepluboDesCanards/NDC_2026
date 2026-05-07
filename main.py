9a6ceed21a8cfcb2fada221a35fee984c8ac3a1c
Le Plus Beau des Canards <n97583308@gmail.com>
2026-05-07 14:41:41 +0200

    SETUP

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pyxel
 
 class Jeu:
-    def __init__(self):
-        pyxel.init(128, 128, "NDC 2026")
+    def __init__(self, w:int, h:int, fps:int):
+        
+        
+        pyxel.init(w, h, "NDC 2026", fps)
 
-    def update(self):
-        pass
+        pyxel.mouse(True)
+
+        pyxel.run(self.update, self.draw)
+
+
+    def update(self): pass
 
     def draw(self):
-        pass
+        pyxel.cls(0)
+
+if __name__ == "__main__":
+    Jeu(128, 128, 60)
