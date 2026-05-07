64b35105edb54e2acdca03cad2ac7744c76edfe0
Vitaly Langlemez <vitlanglemez@gmail.com>
2026-05-07 15:18:30 +0200

    Add files via upload
    timer

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 import pyxel
 
 class Jeu:
-    def __init__(self, w:int, h:int, fps:int):
-        
-        
-        pyxel.init(w, h, "NDC 2026", fps)
-
+    def __init__(self):
+        self.isGameOver = False
+        self.timer = 180
+        self.w = 128
+        self.h = 128
+        pyxel.init(self.w, self.h, "NDC 2026")
         pyxel.mouse(True)
-
         pyxel.run(self.update, self.draw)
 
+    def update(self):
+        if (pyxel.frame_count % 30) == 0:
+            self.timer -= 1
 
-    def update(self): pass
+        if self.timer == 0:
+            self.isGameOver = True
+
+        if self.isGameOver:
+            pyxel.cls(0)
 
     def draw(self):
         pyxel.cls(0)
+        if not(self.isGameOver):
+            timer_border_x = self.w - 16
+            timer_border_y = 1
+            pyxel.rectb(timer_border_x, timer_border_y, 15, 9, 7)
+            pyxel.text(timer_border_x+2, timer_border_y+2, str(self.timer), 7)
+        else:
+            pyxel.text(self.w // 2 - 15, self.h // 2, "Game Over", 7)
 
-if __name__ == "__main__":
-    Jeu(128, 128, 60)
+Jeu()
